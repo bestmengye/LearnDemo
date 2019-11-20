f8140f7757aa03322e6de2eea64ecd0379a7d84b
mengye <mengyexz@126.com>
2019-11-20 15:46:17 +0800

    init demo

---
+++ src/test/java/com/learn/mengye/LearnDemo/LearnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.mengye.LearnDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
