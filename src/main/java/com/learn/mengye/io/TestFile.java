f13991e05880b7c5626aff630cc8191a5899dabc
mengye <mengyexz@126.com>
2019-12-06 16:40:06 +0800

    使用BufferedReader读取项目中的文件

---
+++ src/main/java/com/learn/mengye/io/TestFile.java
@@ -0,0 +1,53 @@
+package com.learn.mengye.io;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.io.*;
+import java.net.URL;
+import java.util.Random;
+
+/**
+ * @author MengYe
+ * @desc 文件类
+ * @date 2019/12/5 9:40
+ */
+public class TestFile {
+
+    /**
+     * 获取当前项目路径的几种方式
+     */
+    public  void getDemoPath()throws IOException {
+        String path = this.getClass().getResource("").getPath();
+        System.out.println("第一种:"+path);
+        URL xmlpath = this.getClass().getClassLoader().getResource("text.txt");
+        System.out.println("第二种:"+xmlpath);
+        String property = System.getProperty("java.class.path");
+        System.out.println("第三种:"+property);
+        File file=new File("");
+        File canonicalFile = file.getCanonicalFile();
+        System.out.println("第四种:"+canonicalFile);
+        String property1 = System.getProperty("user.dir");
+        System.out.println("第五种:"+property1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        //读取项目中的文本文件
+        File file = new File("src/main/resources/text.txt");
+        if (file.exists()) {
+           /* System.out.println(file.getName());
+            System.out.println(file.getAbsoluteFile());
+            System.out.println(file.getFreeSpace());
+            System.out.println(file.getParentFile());
+            System.out.println(file.getCanonicalFile());*/
+        }
+
+        String str = "";
+        String s ="";
+        BufferedReader reader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/src/main/resources/text.txt"));
+        while ((s=reader.readLine()) != null) {
+            str += s+"\n";
+        }
+        System.out.println(str);
+
+    }
+}
