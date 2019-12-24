84c8f4d61ef238ff7e77c3c78edbffb3af8b3f36
mengye <mengyexz@126.com>
2019-12-24 14:25:19 +0800

    新增连接redis配置

--- src/test/java/com/learn/mengye/LearnDemoApplicationTests.java
+++ src/test/java/com/learn/mengye/LearnDemoApplicationTests.java
@@ -1,13 +1,42 @@
 package com.learn.mengye;
 
+import com.alibaba.fastjson.serializer.ObjectArrayCodec;
+import com.alibaba.fastjson.serializer.ObjectSerializer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class LearnDemoApplicationTests {
 
+	@Autowired
+	private StringRedisTemplate redis;
+
 	@Test
 	void contextLoads() {
+		redis.opsForValue().set("mengye","孟野");
 	}
+
+	@Test
+	public void redisHandleList() {
+		List<String> list=new ArrayList<>();
+		list.add("mengye");
+		list.add("gengxiang");
+		list.add("xiaoheng");
+		list.add("chengfeng");
+		//redis左边写入一个List  返回所有集合数量
+		Long count = redis.opsForList().leftPushAll("testList", list);
+		//redis从list集合左边写入一个String  返回所有集合数量
+		Long push = redis.opsForList().leftPush("testList", "111111111111111");
+		System.out.println(push);
+
+	}
+
+
+
 
 }
